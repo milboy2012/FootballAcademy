31514dda6a8db0971c5b26d9beb8c933395d1a56
Андрей Полетило <milboy2012@outlook.com>
2026-08-23 18:23:00 +0300

    AddIdentityContext

--- UI/Program.cs
+++ UI/Program.cs
@@ -14,10 +14,11 @@ namespace UI
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
-            builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+            //builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            //builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<Context>();
+            builder.Services.AddRazorPages();
             builder.Services.AddControllersWithViews();
 
             //строка подключения к базе данных postgresql
